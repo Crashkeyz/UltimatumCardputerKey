a636d58aaebed6c1d1fc1b646b0310e69a67edd5
Crashkeyz <crashkeyz@gmail.com>
2025-12-26 04:45:29 -0800

    Fix SD.h include error - move to cpp file only

--- drivers/cardputer_adv_driver.h
+++ drivers/cardputer_adv_driver.h
@@ -1,10 +1,9 @@
 #ifndef CARDPUTER_ADV_DRIVER_H
 #define CARDPUTER_ADV_DRIVER_H
 
-#include <SD.h>
-#include <FS.h>
-
+// Forward declarations - actual includes in .cpp file
 void initialize_driver();
+void driver_loop();
 bool initialize_sd_card();
 
 #endif // CARDPUTER_ADV_DRIVER_H
